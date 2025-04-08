2a3826cecab2fd1cbbbca6c91ee1f726b9e69692
Демаков Дмитрий Максимович <Demakov-DM@edu.ntiustu.local>
2025-04-08 10:19:30 +0500

    Test send img

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import telebot
 import config
-
+from urllib.request import urlopen
 
 bot = telebot.TeleBot(config.TG_API_TOKEN)
 
-
+@bot.message_handler(commands=['start'])
+def get_user_photo(message):
+    if message.text == '/start':
+        photo1 = urlopen('https://cdn.culture.ru/images/69b64b45-dc21-5230-951c-9722db631cd0')
+        bot.send_photo(message.chat.id, photo1)
 
 bot.infinity_polling()
